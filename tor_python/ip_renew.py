cf77bee40f103f18fbb6c029a4ecb87df8101e5b
weiwongfaye <weiwongfaye@gmail.com>
2017-02-17 12:01:16 +0800

    iprenews and cmdline interpreters

--- tor_python/ip_renew.py
+++ tor_python/ip_renew.py
@@ -1,7 +1,8 @@
 from TorCtl import TorCtl
 import urllib2
  
-user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+user_agent = 'Mozilla/5.0 (Windows; U;\
+ Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 headers={'User-Agent':user_agent}
  
 def request(url):
